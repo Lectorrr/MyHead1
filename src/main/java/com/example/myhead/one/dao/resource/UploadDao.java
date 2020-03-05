7251840d49ad9a2764a178cbb10a4082ddd083a2
Lectorrr <xuesihui0212@gmail.com>
2020-03-05 10:56:20 +0800

    1、删除upload实体表

--- src/main/java/com/example/myhead/one/dao/resource/UploadDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.myhead.one.dao.resource;
-
-import com.example.myhead.one.base.BaseDao;
-import com.example.myhead.one.entity.resource.Upload;
-
-public interface UploadDao extends BaseDao<Upload, String> {
-}
--- src/main/java/com/example/myhead/one/service/resource/UploadService.java
+++
@@ -1,9 +0,0 @@
-package com.example.myhead.one.service.resource;
-
-import com.example.myhead.one.base.BaseService;
-import com.example.myhead.one.entity.resource.Upload;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UploadService extends BaseService<Upload> {
-}
